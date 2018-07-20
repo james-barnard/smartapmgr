53eed797b7460d0cbeebb2957db4290cfcc75c5c
James Barnard <James.Barnard.CO@gmail.com>
2018-07-20 14:13:56 -0500

    adds avg methods to load

--- app/models/load.rb
+++ app/models/load.rb
@@ -14,4 +14,19 @@ class Load < ApplicationRecord
   def ounces_remaining
     ounces.to_i - total_poured
   end
+
+  def days_loaded
+    days = (((removed_at || Time.now) - loaded_at) / 1.day).round
+  end
+
+  def avg_daily
+    days = days_loaded
+    days > 0 ? days : 1
+    total_poured / days_loaded
+  end
+
+  def avg_size
+    count = pours.count
+    total_poured / (count > 0 ? count : 1)
+  end
 end
